5e7b372b2a2649444280cddf105d29fda6d277d2
herman-nott <mcgerman2006@gmail.com>
2025-09-04 15:05:06 +0200

    adadad

--- testfiles/drop_tables.sql
+++
@@ -1,8 +0,0 @@
-USE usof_db;
-
-DELETE FROM users;
--- DELETE FROM posts;
--- DELETE FROM categories;
--- DELETE FROM post_categories;
--- DELETE FROM comments;
--- DELETE FROM likes;
--- testfiles/select.sql
+++
@@ -1,9 +0,0 @@
-USE usof_db;
-
-SELECT * FROM users;
--- SELECT * FROM categories;
--- SELECT * FROM posts;
--- SELECT * FROM post_categories;
--- SELECT * FROM comments;
--- SELECT * FROM likes;
--- SELECT * FROM password_resets;
